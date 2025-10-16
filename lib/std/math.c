3602dd60d8b73747afacabbfece0ec1612cb580a
Duncan Steele <steeleduncan@hotmail.com>
2025-10-17 00:04:40 +0100

    stdlib math functions for normal random

--- lib/std/math.c
+++ lib/std/math.c
@@ -1,5 +1,6 @@
 #include "eyot-runtime-cpu.h"
 #include <math.h>
+#include <stdlib.h>
 
 EyFloat64 ey_stdlib_sqrtd(EyExecutionContext *ctx, EyFloat64 val) {
     return sqrt(val);
@@ -9,6 +10,30 @@ EyFloat64 ey_stdlib_expd(EyExecutionContext *ctx, EyFloat64 val) {
     return exp(val);
 }
 
+EyFloat64 ey_stdlib_logd(EyExecutionContext *ctx, EyFloat64 val) {
+    return log(val);
+}
+
+EyFloat64 ey_stdlib_cosd(EyExecutionContext *ctx, EyFloat64 val) {
+    return cos(val);
+}
+
+EyFloat64 ey_stdlib_sind(EyExecutionContext *ctx, EyFloat64 val) {
+    return sin(val);
+}
+
+EyFloat64 ey_stdlib_tand(EyExecutionContext *ctx, EyFloat64 val) {
+    return tan(val);
+}
+
 EyFloat32 ey_stdlib_sqrtf(EyExecutionContext *ctx, EyFloat32 val) {
     return sqrtf(val);
+}
+
+EyInteger ey_stdlib_rand(EyExecutionContext *ctx) {
+    return rand();
+}
+
+EyInteger ey_stdlib_rand_max(EyExecutionContext *ctx) {
+    return RAND_MAX;
 }
